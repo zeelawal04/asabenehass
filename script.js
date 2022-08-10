75919ea1731079a49d14bd3b554bf4f17b9b99e5
Zainab Lawal <zainabdamilolalawal@gmail.com>
2022-08-10 13:14:02 +0100

    feat:Asabenehass

--- script.js
+++ script.js
@@ -2,20 +2,4 @@
 //no1
 let string =
   "Love is the best thing in this world. Some found their love and some are still looking for their love.";
-let chars = {};
-console.log(arr);
-
-//no2
-let word =
-  "You cannot end a sentence with because because because is a conjunction";
-console.log(word.match("because"));
-
-//no3
-let quote =
-  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
-let result1 = quote.replace(/[%#@$&]/g, " ");
-console.log(result1);
-
-//no4
-let num =
-  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+console.log(string.toLowerCase().match(/love/g)).length;
